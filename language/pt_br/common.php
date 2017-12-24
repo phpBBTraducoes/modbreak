453467892ad4de149373efb8180462f32cc1b224
phpBBTraducoes <eunaumtenhoid@outlook.com>
2017-12-24 15:04:51 -0200

    Traducao ADD

---
+++ language/pt_br/common.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *
+ * Modbreak [Brazilian Portuguese [pt_br]]
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.1.0] (https://github.com/phpBBTraducoes)
+ * @copyright (c) 2017 Ger Bruinsma
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'MODBREAK_HEAD'	=> 'Mensagem do moderador ',
+));
