15a378c68e388f506b06153e422f25c2edeb5eae
eunaumtenhoid <eunaumtenhoid@outlook.com>
2018-08-26 08:11:44 -0300

    update

---
+++ language/pt_br (MBX)/common.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *
+ * Modbreak
+ *
+ * @copyright (c) 2017 Ger Bruinsma
+ * @copyright (c) 2018 LukeWCS
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * Lang_iso   : pt_br
+ * Lang_ver   : 1.2.0
+ * Lang_author: eunaumtenhoid 2018 (https://github.com/phpBBTraducoes)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// Some characters you may want to copy&paste:
+// ’ » „ “ — …
+$lang = array_merge($lang, array(
+	'MODBREAK_HEAD'				=> 'Mensagem do moderador',
+	'MODBREAK_HEAD_FROM'			=> ' de: ',
+	'MODBREAK_HEAD_DATE_SEPARATOR'		=> ' » ',
+	'MODBREAK_BUTTON_TOOLTIP'		=> 'Insira a mensagem do moderador no post: [mod]Texto[/mod]     Só pode ser usado por moderadores',
+));
